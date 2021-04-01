e1d74ad1ffca65a441000ca31166e639448b0e50
Mr-Developer2204 <59203865+Mr-Developer2204@users.noreply.github.com>
2021-04-01 10:18:00 +0530

    Changes in working tree and added Switcher

---
+++ src/components/Switcher/Switcher.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
+import {Balance,Expenses,Landing,News} from './components/pages';
+export default function Switcher() {
+    return (
+        <Route>
+
+        </Route>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Landing from './Landing';
+import Footer from './components/Footer/Footer'
+import Landing from './components/pages/Landing/Landing';
+import NBar from './components/Navbar/Navbar';
 ReactDOM.render(
   <React.StrictMode>
+    <NBar />
     <Landing />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
