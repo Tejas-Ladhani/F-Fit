8f0fbff0c81ad0d3c801134cc32b30b1ade36b8a
Tejas Ladhani <67834407+Tejas-Ladhani@users.noreply.github.com>
2021-06-05 12:13:53 +0530

    Optimised imports

--- src/index.js
+++ src/index.js
@@ -3,20 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NBar from './components/comp/Navbar/Navbar';
-import Footer from './components/comp/Footer/Footer';
+import { NBar } from './components/comp';
 import { UserContextProvider } from './components/contexts/user';
 
 ReactDOM.render(
   <React.StrictMode>
-    <UserContextProvider>   
-    <NBar />
-
-
+    <UserContextProvider>
+      <NBar />
     </UserContextProvider>
-
-    {/* Removed Footer for now , still there in comp */}
-   
   </React.StrictMode>,
 
   document.getElementById('root')
